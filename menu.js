11efad607ad749ad9f8e45269d65bf36c4ac6887
AakashKathait <92093691+AakashKathait@users.noreply.github.com>
2022-01-08 12:51:19 -0800

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,47 @@
+        //selectors
+        const menuNav = document.querySelector('.menu-nav')
+        const menuItems = document.querySelector('.menu-items')
+        const sides = document.querySelector('#side')
+        const entrees = document.querySelector('#entree')
+        const baked = document.querySelector('#baked')
+
+        //Window scroll functions
+        window.addEventListener('scroll', function() {
+
+            function scroll(element, className, moreThan) {
+                async function insideScroll() {
+                        await element.classList.toggle(className, window.scrollY >= moreThan)
+                }
+                insideScroll();
+                }
+
+            scroll(menuNav, 'sticky', 610);
+            scroll(menuItems, 'spacing', 610);
+    
+            function allscroll(element, delayTime, moreThan, lessThan) {
+                    setTimeout(async function() {
+                            await element.classList.toggle('top-hat', window.scrollY > moreThan && window.scrollY < lessThan)
+                            if (window.scrollY > moreThan && window.scrollY < lessThan) {
+                                    element.classList.remove('menu-nav-items')    
+                            }else {
+                                    element.classList.add('menu-nav-items')
+                            }
+                    }, delayTime);
+            }
+    
+            allscroll(sides, 500, 620, 1200);
+            allscroll(entrees, 500, 1200, 2100);
+            allscroll(baked, 500, 2100, 4000)
+    
+            })
+            //Click Fuctions
+            function allClick(element) {
+            element.addEventListener('click', () => {
+                    element.classList.remove('menu-nav-items')
+                    element.classList.add('top-hat')
+            })
+            }
+    
+            allClick(sides);
+            allClick(entrees);
+            allClick(baked);
---
+++ script.js
@@ -0,0 +1,109 @@
+window.onload = function () {
+
+        //Selectors
+        const navbar = document.querySelector('.navbar')
+        const burger = document.querySelector('.hamburger')
+        const nav = document.querySelector('.nav')
+        const home = document.querySelector('.nav-home')        
+        const menu = document.querySelector('.nav-menu')        
+        const about = document.querySelector('.nav-about')        
+        const contact = document.querySelector('.nav-contact')        
+        const body = document.querySelector('body')
+        const submit = document.querySelector('.submit')
+        const contactForm = document.querySelector('#contact-form')
+        const name = document.querySelector('.name')
+        const email = document.querySelector('.email')
+        const subject = document.querySelector('.subject')
+        const message = document.querySelector('.message')
+
+        //Nav
+        burger.addEventListener('click', () => {
+
+                //toggle nav
+                nav.classList.toggle('nav-active')       
+
+
+                //animate in               
+                function linkAnimation(element, index, element2, element3, element4) {
+
+                        function outsideFunc() {                        
+                                        if(element.style.animation) {
+                                                element.style.removeProperty('animation')
+                                        }else {
+                                         element.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                                        }
+                        }
+                        outsideFunc()
+                        
+                                element.addEventListener('click', () => {
+                                        nav.classList.remove('nav-active')       
+
+                                        burger.classList.remove('toggle');
+
+                                        element.style.removeProperty('animation')
+                                        element2.style.removeProperty('animation')
+                                        element3.style.removeProperty('animation')
+                                        element4.style.removeProperty('animation')
+
+                                        body.style.overflow =''
+                                })
+                }
+
+                linkAnimation(home, 0, menu, about, contact);
+                linkAnimation(menu, 1, about, contact, home);
+                linkAnimation(about, 2, contact, home, menu);
+                linkAnimation(contact, 3, home, menu, about);
+
+
+
+                //burger animation
+                burger.classList.toggle('toggle');
+                
+                //hiding scroll
+                if (body.style.overflow == 'hidden'){
+                        body.style.overflow =''
+                        }else {
+                                body.style.overflow ='hidden'
+                        }
+
+        })
+
+        
+        //Window scroll functions
+        window.addEventListener('scroll', function() {
+
+                function scroll(element, className, moreThan) {
+                async function insideScroll() {
+                        await element.classList.toggle(className, window.scrollY >= moreThan)
+                }
+                insideScroll();
+                }
+        
+                scroll(navbar, 'back-color', 70);
+        })
+
+        //Contact Submit
+        contactForm.addEventListener('submit', (e) => {
+                e.preventDefault();
+                sendEmail();
+                name.value = ''
+                email.value = ''
+                subject.value = ''
+                message.value = ''
+        })
+
+        function sendEmail() {
+                Email.send({
+                        Host : "smtp.gmail.com",
+                        Username : "aakashkathait02@gmail.com",
+                        Password : "Devpurposes@4659",
+                        To : 'aakashkathait01@gmail.com',
+                        From : email.value,
+                        Subject : subject.value,
+                        Body : `Name: ${name.value} <br>
+                                Message: ${message.value}`
+                    }).then(
+                      message => alert('Message Sent!')
+                    );
+        }
+}
