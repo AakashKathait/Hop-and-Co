a02b6528c99e2a84c55a4e1bcdd59449bceff192
AakashKathait <92093691+AakashKathait@users.noreply.github.com>
2022-01-20 10:19:10 -0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -40,20 +40,18 @@
                     element.classList.remove('menu-nav-items')
                     element.classList.add('top-hat')
 
-                    function allscroll(element, delayTime, moreThan, lessThan) {
-                        setTimeout(async function() {
-                                await element.classList.toggle('top-hat', window.scrollY > moreThan && window.scrollY < lessThan)
+                        function allscroll(element, delayTime, moreThan, lessThan) {
                                 if (window.scrollY > moreThan && window.scrollY < lessThan) {
-                                        element.classList.remove('menu-nav-items')    
-                                }else {
-                                        element.classList.add('menu-nav-items')
+ 
+                                }else{
+                                        element.classList.remove('top-hat')   
                                 }
-                        }, delayTime);
-                }
-        
-                allscroll(sides, 500, 620, 1200);
-                allscroll(entrees, 500, 1200, 2100);
-                allscroll(baked, 500, 2100, 4000)
+                        }
+
+                                                                                
+                        allscroll(sides, 500, 620, 1200);
+                        allscroll(entrees, 500, 1200, 2100);
+                        allscroll(baked, 500, 2100, 4000)
             })
             }
     
