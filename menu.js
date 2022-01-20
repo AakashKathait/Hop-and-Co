065ce5d93d69a2c3c7bee51ff9a27ae6b35eb85b
AakashKathait <92093691+AakashKathait@users.noreply.github.com>
2022-01-20 11:01:20 -0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -34,29 +34,16 @@
             allscroll(baked, 500, 2100, 4000)
     
             //Click Fuctions
-            function allClick(element) {
+            function allClick(element, moreThan, lessThan) {
             element.addEventListener('click', async function() {
                     await element.classList.remove('menu-nav-items')
                     await element.classList.add('top-hat') 
 
-                        function allscroll(element, delayTime, moreThan, lessThan) {
-                                setTimeout(async function() {
-                                        await element.classList.toggle('top-hat', window.scrollY > moreThan && window.scrollY < lessThan)
-                                        if (window.scrollY > moreThan && window.scrollY < lessThan) {
-                                                element.classList.remove('menu-nav-items')    
-                                        }else {
-                                                element.classList.add('menu-nav-items')
-                                        }
-                                }, delayTime);
-                        }
-                
-                        allscroll(sides, 500, 620, 1200);
-                        allscroll(entrees, 500, 1200, 2100);
-                        allscroll(baked, 500, 2100, 4000)
+                    await element.classList.remove('top-hat', window.scrollY < moreThan && window.scrollY > lessThan)
             })
             }
     
-            allClick(sides);
-            allClick(entrees);
-            allClick(baked);
+            allClick(sides, 620, 1200);
+            allClick(entrees, 1200, 2100);
+            allClick(baked, 2100, 4000);
         })
